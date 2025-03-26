e0fda324e4a52028ca7d3e8ecd67564c40793b07
Jonas Andersen <Jonas@anders1.com>
2025-03-26 20:26:08 +0100

    UPT: NEWEST

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -8,12 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: "wagewise-94eb0.firebaseapp.com",  
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
